54643d24acd433a5bf43fee0f5f0e8960659dcf3
DomenJhanineB <jhanine.domen@dbtc-cebu.edu.ph>
2026-08-19 21:45:50 +0800

    feat: handle incomplete profile data

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,73 @@ void main() {
   runApp(const MyApp());
 }
 
+class Profile {
+  final String? image;
+  final String? name;
+  final String? courseSection;
+  final int? age;
+  final String? hobby;
+
+  const Profile({
+    this.image,
+    this.name,
+    this.courseSection,
+    this.age,
+    this.hobby,
+  });
+}
+
+final List<Profile> profiles = [
+  Profile(
+    image: 'assets/images/Profile Image 1.png',
+    name: 'Jhanine Domen',
+    courseSection: 'BSIT-3',
+    age: 20,
+    hobby: 'Doing Makeups',
+  ),
+
+  Profile(
+    image: 'assets/images/Profile Image 2.png',
+    name: 'Karol Rphael Pal',
+    courseSection: 'BSIT-3',
+    age: 21,
+    hobby: 'Playing Mobile Legends',
+  ),
+
+  Profile(
+    image: 'assets/images/Profile Image 3.png',
+    name: 'Jan Liebert Tabares',
+    courseSection: 'BTVETD-2',
+    age: 20,
+    hobby: null,
+  ),
+
+  Profile(
+    image: 'assets/images/Profile Image 4.png',
+    name: 'Vanneza Jane Domen',
+    courseSection: null,
+    age: 18,
+    hobby: 'Dancing',
+  ),
+
+  Profile(
+    image: 'assets/images/Profile Image 5.png',
+    name: null,
+    courseSection: 'Review for LPT',
+    age: 22,
+    hobby: 'Watching Movies',
+  ),
+];
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
   }
 }
 
@@ -19,231 +80,89 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter Capture the Flag 2: Widgets')),
+      appBar: AppBar(
+        title: const Text('Flutter Capture the Flag 3: Data-Driven UI'),
+      ),
 
       body: SingleChildScrollView(
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-
             children: [
-              Card(
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 40,
-                      backgroundColor: const Color.from(
-                        alpha: 1,
-                        red: 0.263,
-                        green: 0.404,
-                        blue: 0.616,
-                      ),
-                      child: Text(
-                        'DB',
-                        style: TextStyle(
-                          fontSize: 24,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 15),
-                    Text(
-                      'Domen Jhanine B.',
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: const Color.from(
-                          alpha: 1,
-                          red: 0.263,
-                          green: 0.404,
-                          blue: 0.616,
-                        ),
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.2,
-                        shadows: [Shadow(blurRadius: 3, offset: Offset(1, 1))],
-                      ),
-                    ),
+              const SizedBox(height: 20),
 
-                    SizedBox(height: 10),
-
-                    Text(
-                      'BSIT-3',
-                      style: TextStyle(
-                        fontSize: 22,
-                        color: const Color.from(
-                          alpha: 1,
-                          red: 0.263,
-                          green: 0.404,
-                          blue: 0.616,
-                        ),
-                        fontWeight: FontWeight.w600,
-                        letterSpacing: 0.8,
-                      ),
-                    ),
-
-                    SizedBox(height: 15),
-
-                    Text(
-                      'My First Flutter App',
-                      style: TextStyle(
-                        fontSize: 21,
-                        color: const Color.from(
-                          alpha: 1,
-                          red: 0.263,
-                          green: 0.404,
-                          blue: 0.616,
-                        ),
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.w500,
-                        letterSpacing: 0.5,
-                      ),
-                    ),
-
-                    SizedBox(height: 15),
-
-                    Text(
-                      'August 11, 2026',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: const Color.from(
-                          alpha: 1,
-                          red: 0.263,
-                          green: 0.404,
-                          blue: 0.616,
-                        ),
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-
-                    SizedBox(height: 15),
-
-                    Text(
-                      'Fact About Me: I Love Doing Makeups',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: const Color.from(
-                          alpha: 1,
-                          red: 0.404,
-                          green: 0.404,
-                          blue: 0.616,
-                        ),
-                        fontWeight: FontWeight.w600,
-                        letterSpacing: 0.4,
-                      ),
-                    ),
-
-                    SizedBox(height: 18),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-
-                      children: [
-                        Text(
-                          'Age: 20',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: const Color.from(
-                              alpha: 1,
-                              red: 0.263,
-                              green: 0.404,
-                              blue: 0.616,
-                            ),
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-
-                        SizedBox(width: 30),
-
-                        Text(
-                          'Birth Date: July 22, 2006',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: const Color.from(
-                              alpha: 1,
-                              red: 0.263,
-                              green: 0.404,
-                              blue: 0.616,
-                            ),
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    SizedBox(height: 15),
-                  ],
+              const Text(
+                'Five Profiles',
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Color.fromRGBO(67, 103, 157, 1),
+                  fontWeight: FontWeight.bold,
                 ),
               ),
 
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
+              ProfileCard(profile: profiles[0]),
+
+              const SizedBox(height: 20),
+              ProfileCard(profile: profiles[1]),
+
+              const SizedBox(height: 20),
+              ProfileCard(profile: profiles[2]),
+
+              const SizedBox(height: 20),
+              ProfileCard(profile: profiles[3]),
+
+              const SizedBox(height: 20),
+              ProfileCard(profile: profiles[4]),
+
+              const SizedBox(height: 30),
 
               Card(
                 child: Column(
                   children: [
-                    Text(
+                    const SizedBox(height: 15),
+
+                    const Text(
                       'My Favorites',
                       style: TextStyle(
                         fontSize: 26,
-                        color: const Color.from(
-                          alpha: 1,
-                          red: 0.263,
-                          green: 0.404,
-                          blue: 0.616,
-                        ),
+                        color: Color.fromRGBO(67, 103, 157, 1),
                         fontWeight: FontWeight.bold,
                       ),
                     ),
 
-                    SizedBox(height: 15),
+                    const SizedBox(height: 15),
 
-                    Text(
-                      'Favorite Things i do: Make ups',
+                    const Text(
+                      'Favorite Things I Do: Make Ups',
                       style: TextStyle(
                         fontSize: 18,
-                        color: const Color.from(
-                          alpha: 1,
-                          red: 0.263,
-                          green: 0.404,
-                          blue: 0.616,
-                        ),
+                        color: Color.fromRGBO(67, 103, 157, 1),
                       ),
                     ),
 
-                    SizedBox(height: 10),
+                    const SizedBox(height: 10),
 
-                    Text(
+                    const Text(
                       'Favorite Food: Ham',
                       style: TextStyle(
                         fontSize: 18,
-                        color: const Color.from(
-                          alpha: 1,
-                          red: 0.263,
-                          green: 0.404,
-                          blue: 0.616,
-                        ),
+                        color: Color.fromRGBO(67, 103, 157, 1),
                       ),
                     ),
 
-                    SizedBox(height: 10),
+                    const SizedBox(height: 10),
 
-                    Text(
+                    const Text(
                       'Favorite Brand Shoes: Adidas',
                       style: TextStyle(
                         fontSize: 18,
-                        color: const Color.from(
-                          alpha: 1,
-                          red: 0.263,
-                          green: 0.404,
-                          blue: 0.616,
-                        ),
+                        color: Color.fromRGBO(67, 103, 157, 1),
                       ),
                     ),
 
-                    SizedBox(height: 18),
+                    const SizedBox(height: 18),
 
-                    Row(
+                    const Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
@@ -266,14 +185,89 @@ class MyHomePage extends StatelessWidget {
                       ],
                     ),
 
-                    SizedBox(height: 15),
+                    const SizedBox(height: 15),
                   ],
                 ),
               ),
 
-              SizedBox(height: 20),
+              const SizedBox(height: 30),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  final Profile profile;
+
+  const ProfileCard({super.key, required this.profile});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+
+        child: Column(
+          children: [
+            profile.image != null
+                ? Image.asset(
+                    profile.image!,
+                    width: 150,
+                    height: 150,
+                    fit: BoxFit.cover,
+                  )
+                : const Icon(Icons.person, size: 150),
+
+            Text(
+              profile.name ?? 'Name not provided',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 30,
+                color: Color.fromRGBO(67, 103, 157, 1),
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.2,
+                shadows: [Shadow(blurRadius: 3, offset: Offset(1, 1))],
+              ),
+            ),
+
+            const SizedBox(height: 10),
+            Text(
+              profile.courseSection ?? 'Course: Unknown',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 22,
+                color: Color.fromRGBO(67, 103, 157, 1),
+                fontWeight: FontWeight.w600,
+                letterSpacing: 0.8,
+              ),
+            ),
+
+            const SizedBox(height: 15),
+            Text(
+              'Age: ${profile.age ?? 'Not provided'}',
+              style: const TextStyle(
+                fontSize: 18,
+                color: Color.fromRGBO(67, 103, 157, 1),
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+            Text(
+              'Hobby: ${profile.hobby ?? 'Not provided'}',
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 18,
+                color: Color.fromRGBO(67, 103, 157, 1),
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+
+            const SizedBox(height: 15),
+          ],
         ),
       ),
     );
